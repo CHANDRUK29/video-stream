f0daad5b73b4da2e23bce5156b6142b9e8e885b3
chandru <chandruviru143@gmail.com>
2024-08-23 07:40:56 +0530

    video rendering base code set

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,38 @@
-import CardMedia from '@mui/material/CardMedia';
+
 
 const App = () => {
   return (
     <>
       <h1>Test</h1>
-      {/* <CardMedia src='http://localhost:6000/videos/'>
-
-      </CardMedia> */}
+      {/* <div> */}
+        <p>video-stream</p>
       <video width='480' height='240' controls autoPlay>
-        <source src="https://www.youtube.com/watch?v=gk6Jri1kGTs"
+        <source src="http://localhost:5500/video-stream"
           type='video/mp4' />
         Your browser does not support video
       </video>
+
+      {/* </div> */}
+      {/* <div> */}
+        <p>videos</p>
+      <video width='480' height='240' controls autoPlay>
+        <source src="http://localhost:5500/videos"
+          type='video/mp4' />
+        Your browser does not support video
+      </video>
+      {/* </div> */}
+
+      {/* <div> */}
+        <p>videoplayer</p>
+      <video width='480' height='240' controls autoPlay>
+        <source src="http://localhost:5500/videoplayer"
+          type='video/mp4' />
+        Your browser does not support video
+      </video>
+      {/* </div> */}
+
+      {/* <video src="http://localhost:5500/video-stream" width="1080px"
+        controls></video> */}
     </>
   )
 }
--- server/server.js
+++ server/server.js
@@ -6,31 +6,37 @@ const app = express();
 app.use(cors());
 
 
-app.get('/test', (req, res) => res.send('it is healthy'))
+app.get('/health-check', (req, res) => res.send('it is healthy'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
 
 app.get('/videos', (req, res) => {
     try {
-        const filePath = 'videos/video1.mp4'
-        // const filePath = 'videos/video2.mp4'
+        const filePath = './videos/video2.mp4'        // const filePath = 'videos/video2.mp4'
         if (!filePath) return res.status(404).json({ message: "file not found" })
 
-        //The fs.stat()==> function synchronously retrieves information about a file or directory
+        //The fs.statsync()==> function synchronously retrieves information about a file or directory
         const stat = fs.statSync(filePath);
         //file size 
         const fileSize = stat.size;
         //finding range
         const range = req.headers.range;
+        console.log(range)
         //from range ==> we have to fetch parts from parts ==> we can get start and end
 
         if (range) {
             //usually range will be ==> bytes=200-1000,2000-6576,19000- etc...
             // we need to remove bytes and get start and end
-            const parts = range.replace('/bytes=/', '').split('-');
-            console.log(parts);
-            const start = parseInt(parts[0], 10);
-            const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+            // const parts = range.replace('/bytes=/', ' ').split('-');
+            // console.log(parts);
+            const chunckSize = 10 ** 6;
+            const start = Number(range.replace(/\D/g, ""))
+            console.log(start);
+            // const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+            const end = Math.min(start + chunckSize, fileSize - 1);
 
-            const chunckSize = end - start + 1
 
             const file = fs.createReadStream(filePath, { start, end });
             //neccessary header options
@@ -59,12 +65,91 @@ app.get('/videos', (req, res) => {
         return res.status(500).json({ error: error.message })
     }
 
-
-
 })
+
+
+
+app.get("/video-stream", function (req, res) {
+    //finding range
+    //from range ==> we have to fetch parts from parts ==> we can get start and end
+    const range = req.headers.range;
+    console.log("range",range)
+    if (!range) {
+        return res.status(400).send("Requires Range header");
+    }
+    const videoPath = './videos/video2.mp4'
+
+    //The fs.statsync()==> function synchronously retrieves information about a file or directory
+    const videoSize = fs.statSync("videos/video1.mp4").size;
+    console.log("videosize",videoSize)
+    const CHUNK_SIZE = 1 * 1e6;
+
+    //usually range will be ==> bytes=200-1000,2000-6576,19000- etc...
+    // we need to remove bytes and get start and end
+    const start = Number(range.replace(/\D/g, ""));
+    console.log("start",start)
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+    console.log("end",start)
+    const contentLength = end - start + 1;
+    console.log("content length",start)
+    //neccessary header options
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4",
+    };
+
+    // we are using 206 to say that it is a partial content
+    res.writeHead(206, headers);
+    const videoStream = fs.createReadStream(videoPath, { start, end });
+    videoStream.pipe(res);
+});
+
+
+app.get('/videoplayer', (req, res) => {
+    //finding range
+    //from range ==> we have to fetch parts from parts ==> we can get start and end
+    const range = req.headers.range
+    console.log("range",range)
+    //mentioning the path of video to be played
+    const videoPath = './videos/video2.mp4';
+
+    //getting video size and setting chunk size
+    //The fs.statsync()==> function synchronously retrieves information about a file or directory
+    const videoSize = fs.statSync(videoPath).size
+    console.log("videosize",videoSize)
+    const chunkSize = 1 * 1e6;
+
+     //usually range will be ==> bytes=200-1000,2000-6576,19000- etc...
+    // we need to remove bytes and get start and end
+    const start = Number(range.replace(/\D/g, ""))
+    console.log("start",start)
+    const end = Math.min(start + chunkSize, videoSize - 1)
+    console.log("end",start)
+    const contentLength = end - start + 1;
+    console.log("content length",start)
+    //neccessary header options
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4"
+    }
+
+    // we are using 206 to say that it is a partial content
+    res.writeHead(206, headers)
+    const stream = fs.createReadStream(videoPath, {
+        start,
+        end
+    })
+    stream.pipe(res)
+})
+
 
 
 const PORT = 5500;
 app.listen(PORT, () => {
     console.log(`server is listening on port ${PORT}`)
 })
+
